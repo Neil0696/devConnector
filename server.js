9bf93bd65064d6cf89c5d20f1b2ee0449b8f32c7
O'neil Malik <oneilmalik09@gmail.com>
2022-03-16 18:05:05 +0000

    add folders

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+//Connect database
+connectDB();
+
+app.get('/', (req, res) => res.send('API running'));
+
+//Define routes
+app.use('api/users', require('./routes/api/users'))
+// app.use('api/auth', require('./routes/api/auth'))
+app.use('api/profile', require('./routes/api/profile'))
+app.use('api/post', require('./routes/api/post'))
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
